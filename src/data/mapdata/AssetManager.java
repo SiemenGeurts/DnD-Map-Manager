28dc37b614133597567190e1f1e8485bab151460
Siemen Geurts <siemen.geurts@gmail.com>
2019-08-29 15:48:22 +0200

    AssetManager improved

--- src/data/mapdata/AssetManager.java
+++ src/data/mapdata/AssetManager.java
@@ -1,12 +1,23 @@
 package data.mapdata;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javax.imageio.ImageIO;
+import javax.swing.filechooser.FileSystemView;
+
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 
 public class AssetManager {
@@ -14,7 +25,7 @@ public class AssetManager {
 	private static AtomicInteger generator;
 	private static ArrayList<Integer> openSlots;
 	public static HashMap<Integer, Image> textures;
-	
+
 	public static void initializeManager() throws IOException {
 		textures = new HashMap<>();
 		readTexturesFromDisk();
@@ -29,34 +40,67 @@ public class AssetManager {
 			for (int i = 0; i < max; i++) {
 				if (!takenSlots.contains(i))
 					openSlots.add(i);
-			}			
+			}
 		}
 		generator = new AtomicInteger(max + 1);
-		
-		addTexture(new Image("assets/images/tiles/dirt.png"));
-		addTexture(new Image("assets/images/tiles/floor.png"));
-		addTexture(new Image("assets/images/tiles/guard.png"));
-		addTexture(new Image("assets/images/tiles/intruder.png"));
 	}
-	
-	public static void addTexture(Image texture) {
+
+	public static void addTexture(Image texture) throws IOException {
+		int id = 0;
 		if (!openSlots.isEmpty()) {
-			 textures.put(openSlots.remove(0), texture);
+			textures.put(id = openSlots.remove(0), texture);
 		} else
-			textures.put(generator.getAndIncrement(), texture);
+			textures.put(id = generator.getAndIncrement(), texture);
+		saveToFile(id, texture);
 	}
-	
-	public static void removeTexture(Integer id) {
+
+	public static void removeTexture(Integer id) throws IOException {
 		textures.remove(id);
 		openSlots.add(id);
+		removeFile(id);
+	}
+	
+	private static void removeFile(int id) throws IOException {
+		String defaultDirectory = FileSystemView.getFileSystemView().getDefaultDirectory().getPath();
+		new File(defaultDirectory + "/DnD Map Manager/Textures/" + id + ".png").delete();
+		writeTexturesToDisk();
+	}
+
+	private static void saveToFile(int id, Image image) throws IOException {
+		String defaultDirectory = FileSystemView.getFileSystemView().getDefaultDirectory().getPath();
+		File outputFile = new File(defaultDirectory + "/DnD Map Manager/Textures/" + id + ".png");
+		outputFile.mkdirs();
+		BufferedImage bImage = SwingFXUtils.fromFXImage(image, null);
+		ImageIO.write(bImage, "png", outputFile);
+		writeTexturesToDisk();
 	}
 	
 	private static void writeTexturesToDisk() throws IOException {
-		
+		String defaultDirectory = FileSystemView.getFileSystemView().getDefaultDirectory().getPath();
+		FileWriter writer = new FileWriter(new File(defaultDirectory + "/DnD Map Manager/Textures/textureInfo.txt"), false);
+		StringBuilder s = new StringBuilder();
+		for (Integer i : textures.keySet()) {
+			s.append(i + System.lineSeparator());
+		}
+		writer.write(new String(s));
+		writer.close();
 	}
-	
+
 	private static void readTexturesFromDisk() throws IOException {
-		
+		String defaultDirectory = FileSystemView.getFileSystemView().getDefaultDirectory().getPath();
+		File f = new File(defaultDirectory + "/DnD Map Manager/Textures/textureInfo.txt");
+		if (!f.exists())
+			return;
+		InputStream is = new FileInputStream(f);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		String line = reader.readLine();
+		while (line != null) {
+			int id = Integer.valueOf(line);
+			System.out.println(defaultDirectory + "\\DnD Map Manager\\Textures\\" + id + ".png");
+			textures.put(id, new Image("file://" + defaultDirectory + "\\DnD Map Manager\\Textures\\" + id + ".png"));
+			line = reader.readLine();
+		}
+		reader.close();
 	}
-	
+
 }
