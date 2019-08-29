e1b210741d2b0508eff84fe19c2e1d785686e141
Siemen Geurts <siemen.geurts@gmail.com>
2019-08-29 14:54:23 +0200

    AssetManager added

--- src/data/mapdata/AssetManager.java
+++ src/data/mapdata/AssetManager.java
@@ -1,5 +1,6 @@
 package data.mapdata;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -14,19 +15,28 @@ public class AssetManager {
 	private static ArrayList<Integer> openSlots;
 	public static HashMap<Integer, Image> textures;
 	
-	public static void initializeManager() {
+	public static void initializeManager() throws IOException {
+		textures = new HashMap<>();
+		readTexturesFromDisk();
 		ArrayList<Integer> takenSlots = new ArrayList<>();
 		for (Entry<Integer, Image> entry : textures.entrySet()) {
 			takenSlots.add(entry.getKey());
 		}
-		
-		int max = Collections.max(takenSlots);
-		generator = new AtomicInteger(max + 1);
 		openSlots = new ArrayList<>();
-		for (int i = 0; i < max; i++) {
-			if (!takenSlots.contains(i))
-				openSlots.add(i);
+		int max = -1;
+		if (!takenSlots.isEmpty()) {
+			max = Collections.max(takenSlots);
+			for (int i = 0; i < max; i++) {
+				if (!takenSlots.contains(i))
+					openSlots.add(i);
+			}			
 		}
+		generator = new AtomicInteger(max + 1);
+		
+		addTexture(new Image("assets/images/tiles/dirt.png"));
+		addTexture(new Image("assets/images/tiles/floor.png"));
+		addTexture(new Image("assets/images/tiles/guard.png"));
+		addTexture(new Image("assets/images/tiles/intruder.png"));
 	}
 	
 	public static void addTexture(Image texture) {
@@ -39,6 +49,14 @@ public class AssetManager {
 	public static void removeTexture(Integer id) {
 		textures.remove(id);
 		openSlots.add(id);
+	}
+	
+	private static void writeTexturesToDisk() throws IOException {
+		
+	}
+	
+	private static void readTexturesFromDisk() throws IOException {
+		
 	}
 	
 }
