1d9234d9efe1094bd62d772950e9efb1d0e31263
Siemen Geurts <siemen.geurts@gmail.com>
2019-08-29 14:01:06 +0200

    GUI changes

--- src/data/mapdata/Map.java
+++ src/data/mapdata/Map.java
@@ -2,4 +2,32 @@ package data.mapdata;
 
 public class Map {
 
+	private int width, height;
+	private Tile[][] tiles;
+	
+	
+	public Map(int _width, int _height) {
+		width = _width;
+		height = _height;
+	}
+	
+	public void setTile(int x, int y, Tile tile) {
+		tiles[y][x] = tile;
+	}
+	
+	public Tile getTile(int x, int y) {
+		return tiles[y][x];
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public Tile[][] getTiles() {
+		return tiles;
+	}
 }
